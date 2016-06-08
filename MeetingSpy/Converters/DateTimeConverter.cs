2c7e2e1063e94559f232101c1e69117b02aa106b
Saquib Rashid <srashid@microsoft.com>
2016-06-08 15:59:16 -0700

    updated bindings and added datetime converter

---
+++ MeetingSpy/Converters/DateTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MeetingSpy.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            var dateTime = value as DateTimeTimeZone;
+
+            if (dateTime == null)
+                return string.Empty;
+
+            return string.Format("{0} {1}", dateTime.DateTime, dateTime.TimeZone);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
