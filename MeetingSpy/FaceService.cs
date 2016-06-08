0d1e8ca6f1f7af14a749f6b01d06c8d0f3236cb8
Graham Elliott <grahame@microsoft.com>
2016-06-08 15:18:00 -0700

    Added Cognitive Services Face API classes

---
+++ MeetingSpy/FaceService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.ProjectOxford.Face;
+using System.Collections.Specialized;
+
+namespace MeetingSpy
+{
+    class FaceService
+    {
+        public List<Guid> faceIds;
+
+        public async void DetectFaces(string subscriptionID, List<ReferenceFace> referenceFaces)
+        {
+            faceIds = new List<Guid>();
+            FaceServiceClient faceServiceClient = new FaceServiceClient(subscriptionID);
+
+            Microsoft.ProjectOxford.Face.Contract.Face[] faces;
+            int faceCount = 0;
+
+            try
+            {
+                foreach (ReferenceFace refface in referenceFaces)
+                {
+                    faces = await faceServiceClient.DetectAsync(refface.ImageURL);
+
+                    foreach (var face in faces)
+                    {
+                        refface.FaceID = face.FaceId;
+                        faceIds.Add(face.FaceId);
+                        faceCount++;
+                    }
+                }
+
+                Guid[] faceIDGuids = new Guid[faceCount];
+                int count = 0;
+
+                foreach (var faceid in faceIds)
+                {
+                    faceIDGuids[count] = faceid;
+                    count++;
+                }
+
+                var results = await faceServiceClient.FindSimilarAsync(faceIds[0], faceIDGuids);
+
+                foreach (ReferenceFace refface in referenceFaces)
+                {
+                    foreach (var result in results)                
+                     {
+                        if (result.FaceId == refface.FaceID)
+                        {
+                            refface.Confidence = result.Confidence;
+                            break;
+                        }
+                        else
+                        {
+                            refface.Confidence = 0.00;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ MeetingSpy/ReferenceFace.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingSpy
+{
+    class ReferenceFace
+    {
+        private Guid faceID;
+        private string imageURL;
+        private double confidence;
+
+        public ReferenceFace()
+        {
+
+        }
+
+        public ReferenceFace(string _imageURL)
+        {
+            imageURL = _imageURL;
+        }
+
+        public Guid FaceID
+        {
+            get
+            {
+                return faceID;
+            }
+
+            set
+            {
+                faceID = value;
+            }
+        }
+        public string ImageURL
+        {
+            get
+            {
+                return imageURL;
+            }
+
+            set
+            {
+                imageURL = value;
+            }
+        }
+
+        public double Confidence
+        {
+            get
+            {
+                return confidence;
+            }
+
+            set
+            {
+                confidence = value;
+            }
+        }
+    }
+}
