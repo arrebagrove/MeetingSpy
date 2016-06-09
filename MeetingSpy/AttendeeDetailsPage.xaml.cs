46c8bfed208214d7fb9b827804ba4899e4a36a73
Jason Young <jason@ytechie.com>
2016-06-09 08:32:57 -0700

    Show linkedin data as text

--- MeetingSpy/AttendeeDetailsPage.xaml.cs
+++ MeetingSpy/AttendeeDetailsPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace MeetingSpy
 {
@@ -14,5 +8,32 @@ namespace MeetingSpy
         {
             InitializeComponent();
         }
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			IsBusy = true;
+			var linkedInAttendees = await LinkedIn.Search("Richard", "Custance");
+			IsBusy = false;
+
+			DetailLabel.Text = "";
+			foreach (var attendee in linkedInAttendees)
+			{
+				AddText("Name: " + attendee.Name);
+				AddText("Title: " + attendee.Title);
+				AddText("Location: " + attendee.Location);
+				AddText("Last Job: " + attendee.LastJob);
+				AddText("Profile Photo: " + attendee.ProfilePhotoUrl);
+				AddText("Education: " + attendee.Education);
+
+				AddText("-------------");
+			}
+		}
+
+		private void AddText(string newText)
+		{
+			DetailLabel.Text += newText + "\n";
+		}
     }
 }
